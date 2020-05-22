31cc19cd49bf06966935d8188fb9bd01ba30af13
Phil Humphrey <phumphrey588@yahoo.com>
2020-05-22 23:28:59 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,27 @@
+class Song
+  
+    attr_accessor :name, :artist, :genre
+    
+    @@count = 0
+    
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @@artists << artists
+    @genre = genre
+    @@genres << genre
+    @@count += 1
+  end
+  
+ def self.count 
+   @@count
+ end
+ 
+ def self.count
+   @@artists 
+ end
+def self.genres
+  @@genres = []
+  
+end
+end
