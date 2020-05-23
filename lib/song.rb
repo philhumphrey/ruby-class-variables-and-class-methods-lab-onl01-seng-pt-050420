870cda50c42568c0e14af08f4b59974deaf7063a
Phil Humphrey <phumphrey588@yahoo.com>
2020-05-23 00:05:40 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,7 +9,7 @@ class Song
   def initialize(name, artist, genre)
      @@count += 1
      @@genre << genre
-     @@artists << 
+     @@artists << artist
     @name = name
     @artist = artist
     @genre = genre
