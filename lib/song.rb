3ac3c4473a33ff0fee428349f5a4eb60391535b1
Phil Humphrey <phumphrey588@yahoo.com>
2020-05-23 00:29:44 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -27,7 +27,17 @@ def self.count
  def self.genres
    @@genres.uniq
 end
-
+def self.genre_count
+    genre_count = {}
+    @@genres.each do |genre|
+      if genre_count[genre]
+        genre_count[genre] += 1 
+      else
+        genre_count[genre] = 1
+      end
+    end
+    genre_count
+  end
 
 
 end
